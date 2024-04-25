e6c42154a44b47bd895b39b7df80f73d62fdd47b
aaminha <dks4857@gmail.com>
2024-04-25 21:18:06 +0900

    design: 디자인 시스템 typo 추가 (#2)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -53,19 +53,173 @@ interface Font {
 const FONT = ({ font, weight, size, lineHeight }: Font): string => {
   return `
     font-family : "${font}";
-    font-weight : ${weight};
+    font-weight: ${weight};
     font-size : ${size}px;
-    line-height : ${lineHeight}%;
+    line-height : ${lineHeight}px;
     `;
 };
 
 const fonts = {
-  example: FONT({
-    font: '',
-    weight: 0,
-    size: 0,
-    lineHeight: 0,
-  }),
+  desktop: {
+    d1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 700,
+      size: 82,
+      lineHeight: 100,
+    }),
+    d2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 65,
+      lineHeight: 80,
+    }),
+    h1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 700,
+      size: 50,
+      lineHeight: 60,
+    }),
+    h2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 40,
+      lineHeight: 50,
+    }),
+    title1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 700,
+      size: 32,
+      lineHeight: 40,
+    }),
+    title2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 25,
+      lineHeight: 32,
+    }),
+    body1m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 18,
+      lineHeight: 28,
+    }),
+    body1r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 18,
+      lineHeight: 28,
+    }),
+    body2m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 16,
+      lineHeight: 24,
+    }),
+    body2r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 16,
+      lineHeight: 24,
+    }),
+    label1m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 14,
+      lineHeight: 20,
+    }),
+    label1r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 14,
+      lineHeight: 20,
+    }),
+    label2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 12,
+      lineHeight: 20,
+    }),
+  },
+  mobile: {
+    d1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 58,
+      lineHeight: 64,
+    }),
+    d2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 45,
+      lineHeight: 56,
+    }),
+    h1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 36,
+      lineHeight: 44,
+    }),
+    h2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 28,
+      lineHeight: 32,
+    }),
+    title1: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 700,
+      size: 22,
+      lineHeight: 28,
+    }),
+    title2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 16,
+      lineHeight: 24,
+    }),
+    body1m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 16,
+      lineHeight: 24,
+    }),
+    body1r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 16,
+      lineHeight: 24,
+    }),
+    body2m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 14,
+      lineHeight: 20,
+    }),
+    body2r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 14,
+      lineHeight: 20,
+    }),
+    label1m: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 14,
+      lineHeight: 20,
+    }),
+    label1r: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 400,
+      size: 14,
+      lineHeight: 20,
+    }),
+    label2: FONT({
+      font: 'Spoqa Han Sans Neo',
+      weight: 500,
+      size: 12,
+      lineHeight: 20,
+    }),
+  },
 };
 
 export type ColorsTypes = typeof colors;
