3a2eda2030723e038a05dc6ecbb8ee7c37914c34
aaminha <dks4857@gmail.com>
2024-04-26 02:43:27 +0900

    design: 디자인 시스템 breakpoint 추가 (#2)

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,9 +1,10 @@
-import { ColorTypes, FontTypes } from '@/styles/theme/theme';
+import { ColorTypes, DeviceTypes, FontTypes } from '@/styles/theme/theme';
 import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
     color: ColorTypes;
     font: FontTypes;
+    device: DeviceTypes;
   }
 }
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
 import { color } from '@/styles/theme/color';
+import { device } from '@/styles/theme/device';
 import { font } from '@/styles/theme/font';
 
 export type ColorTypes = typeof color;
@@ -8,4 +9,5 @@ export type FontTypes = typeof font;
 export const theme: DefaultTheme = {
   color,
   font,
+  device,
 };
