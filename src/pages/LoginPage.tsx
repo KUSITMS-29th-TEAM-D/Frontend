b65874556f14263905ed2bb458afcfa1feea822c
Mungjin01 <beckaljm@naver.com>
2024-04-25 12:25:15 +0900

    feat: 로그인 페이지 라우터

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import KakaoLoginButton from '@/components/KakaoButton';
+
+export const LoginPage = () => {
+  return (
+    <div>
+      <div>로그인페이지</div>
+      <KakaoLoginButton />
+    </div>
+  );
+};
