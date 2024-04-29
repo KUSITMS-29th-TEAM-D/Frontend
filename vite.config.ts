e7c72ea63d14577c79a2305b47266dc7bc74a597
aaminha <dks4857@gmail.com>
2024-04-29 13:15:04 +0900

    chore: `vite-plugin-svgr` 플러그인 설치 및 세팅 (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import svgr from '@svgr/rollup';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
 });
