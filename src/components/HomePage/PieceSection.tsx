4f6e7bd13e2a44e4964650c0b10aefd1b0ada944
aaminha <dks4857@gmail.com>
2024-05-02 14:35:58 +0900

    feat: PieceSection 컴포넌트 구현 (#9)

---
+++ src/components/HomePage/PieceSection.tsx
@@ -0,0 +1,229 @@
+import styled, { keyframes } from 'styled-components';
+
+import { ReactComponent as ArrowIcon } from '@/assets/icons/arrowDown.svg';
+import { Chips } from '@/components/common/Chips';
+import { CARD_IMAGE } from '@/constants/card';
+import { SectionContainer } from '@/styles';
+
+const Dummy = {
+  piece: '크리에이터',
+  name: '민선',
+  brand: 'creator',
+  chips: [
+    { content: '감성적인', weight: 1.0 },
+    { content: '도전적인', weight: 0.9 },
+    { content: '탐색적인', weight: 0.8 },
+    { content: '활동적인', weight: 0.7 },
+    { content: '열정적인', weight: 0.6 },
+    { content: '현실적인', weight: 0.5 },
+    { content: '관습적인', weight: 0.45 },
+  ],
+};
+
+export const PieceSection = () => {
+  return (
+    <StyledContainer>
+      <StyledSectionContainer>
+        <StyledTitle>
+          <div className="user-info">
+            <span className="highlight">{Dummy.piece} 조각</span>을 가진{' '}
+            <span className="highlight">{Dummy.name}</span>님,
+          </div>
+          <div className="intro">셀피스와 함께 나를 브랜딩해봐요.</div>
+        </StyledTitle>
+        <StyledCardContainer>
+          <StyledCard src={CARD_IMAGE[Dummy.brand]} alt="card" />
+          {Dummy.chips.map((chip, index) => (
+            <StyledBubble key={chip.content} className={`b${index}`} $weight={chip.weight}>
+              <span>{chip.content}</span>
+            </StyledBubble>
+          ))}
+        </StyledCardContainer>
+        <StyledContents>
+          <StyledChipsContainer>
+            {Dummy.chips.map((chip) => (
+              <Chips key={chip.content} primary>
+                {chip.content}
+              </Chips>
+            ))}
+          </StyledChipsContainer>
+          <div>
+            키워드를 가진 <span className="highlight">{Dummy.name}</span>님께
+            <br />
+            이런 경험을 추천드리고 싶어요.
+          </div>
+          <ArrowIcon width="48px" />
+        </StyledContents>
+      </StyledSectionContainer>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.section`
+  background: ${({ theme }) => `linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0) 27%,
+      rgba(232, 221, 254, 0.45) 65%,
+      ${theme.color.primary200} 100%
+    ),
+    ${theme.color.white}`};
+
+  .highlight {
+    color: ${({ theme }) => theme.color.primary600};
+  }
+`;
+
+const StyledSectionContainer = styled(SectionContainer)`
+  padding: 64px;
+  padding-top: 140px;
+
+  text-align: center;
+`;
+
+const StyledTitle = styled.div`
+  .user-info {
+    margin-bottom: 16px;
+
+    ${({ theme }) => theme.font.desktop.h1};
+    color: ${({ theme }) => theme.color.black};
+  }
+
+  .intro {
+    ${({ theme }) => theme.font.desktop.title1};
+    color: ${({ theme }) => theme.color.gray600};
+  }
+`;
+
+const StyledCardContainer = styled.div`
+  width: 100%;
+
+  position: relative;
+  z-index: 0;
+`;
+
+const StyledCard = styled.img`
+  height: 440px;
+  margin: 56px 0;
+`;
+
+const movedown = keyframes`50% {
+    transform: translateY(-7px);
+  }`;
+
+const moveup = keyframes`50% {
+    transform: translateY(7px);
+  }`;
+
+const moveright = keyframes`50% {
+    transform: translateX(7px);
+  }`;
+
+const moveleft = keyframes`50% {
+    transform: translateX(-7px);
+  }`;
+
+const StyledBubble = styled.div<{ $weight: number }>`
+  width: ${({ $weight }) => $weight * 360}px;
+  height: ${({ $weight }) => $weight * 360}px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  will-change: transform;
+
+  color: ${({ theme }) => theme.color.primary800};
+  ${({ theme }) => theme.font.desktop.title1};
+
+  position: absolute;
+  z-index: -1;
+
+  border-radius: 50%;
+  background: radial-gradient(
+    50% 50% at 50% 50%,
+    rgba(255, 255, 255, 0) 76%,
+    rgba(204, 179, 253, 0.4) 100%
+  );
+
+  box-shadow:
+    -0.319px 51.263px 151.773px 0px rgba(48, 13, 115, 0.12),
+    -0.04px 6.444px 21.116px 0px rgba(48, 13, 115, 0.06);
+
+  &.b0 {
+    bottom: 5%;
+    left: 15%;
+
+    animation:
+      ${movedown} 2s ease-in-out infinite,
+      ${moveleft} 3s ease-in-out infinite;
+  }
+
+  &.b1 {
+    bottom: 10%;
+    right: 13%;
+
+    animation:
+      ${movedown} 2s ease-in-out infinite,
+      ${moveright} 2.5s ease-in-out infinite;
+  }
+
+  &.b2 {
+    top: 2%;
+    left: 3%;
+
+    animation:
+      ${movedown} 3s ease-in-out infinite,
+      ${moveleft} 2s ease-in-out infinite;
+  }
+
+  &.b3 {
+    top: 5%;
+    right: 3%;
+
+    animation:
+      ${moveup} 1.8s ease-in-out infinite,
+      ${moveright} 3s ease-in-out infinite;
+  }
+
+  &.b4 {
+    bottom: 10%;
+    right: 0%;
+
+    animation:
+      ${moveup} 2s ease-in-out infinite,
+      ${moveleft} 3s ease-in-out infinite;
+  }
+
+  &.b5 {
+    bottom: 10%;
+    left: 0%;
+
+    animation:
+      ${moveup} 3s ease-in-out infinite,
+      ${moveright} 2.3s ease-in-out infinite;
+  }
+
+  &.b6 {
+    top: 3%;
+    right: 24%;
+
+    animation:
+      ${movedown} 3s ease-in-out infinite,
+      ${moveright} 4s ease-in-out infinite;
+  }
+`;
+
+const StyledChipsContainer = styled.div`
+  display: flex;
+  gap: 16px;
+
+  padding: 30px 0 38px 0;
+`;
+
+const StyledContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+
+  ${({ theme }) => theme.font.desktop.title1};
+`;
---
+++ src/constants/card.ts
@@ -0,0 +1,19 @@
+import CreatorCard from '@/assets/cards/01_creator.svg';
+import InsighterCard from '@/assets/cards/02_insighter.svg';
+import InnovatorCard from '@/assets/cards/03_innovator.svg';
+import InventorCard from '@/assets/cards/04_inventor.svg';
+import ProjectorCard from '@/assets/cards/05_projector.svg';
+import ConnectorCard from '@/assets/cards/06_connector.svg';
+import EncouragerCard from '@/assets/cards/07_encourager.svg';
+import OrganizerCard from '@/assets/cards/08_organizer.svg';
+
+export const CARD_IMAGE: { [key: string]: string } = {
+  creator: CreatorCard,
+  insighter: InsighterCard,
+  innovator: InnovatorCard,
+  inventor: InventorCard,
+  projector: ProjectorCard,
+  connector: ConnectorCard,
+  encourager: EncouragerCard,
+  organizer: OrganizerCard,
+};
