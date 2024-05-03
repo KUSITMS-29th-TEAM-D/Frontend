58b3082bac9bd87aa75e068b909eae1bfd47985c
aaminha <dks4857@gmail.com>
2024-05-03 16:22:40 +0900

    feat: MainLayout 생성 및 router에 적용 (#9)

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { MainLayout } from '@/components/common/Layout/MainLayout';
 import { HomePage } from '@/pages/HomePage';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="" element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
     </Routes>
   );
 };
