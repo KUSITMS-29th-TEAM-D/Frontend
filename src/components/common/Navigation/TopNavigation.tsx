fbf34779071928b1c41544ae7e9ae2cde6221ef6
aaminha <dks4857@gmail.com>
2024-05-03 15:45:14 +0900

    refactor: TopNavigation 컴포넌트 코드 구조 수정 - SmallButton 컴포넌트 삭제 - 불필요한 스타일 컴포넌트 삭제

--- src/components/common/Navigation/TopNavigation.tsx
+++ src/components/common/Navigation/TopNavigation.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 
 import { Link, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { ReactComponent as MenuIcon } from '@/assets/icons/menu.svg';
 import { ReactComponent as UserIcon } from '@/assets/icons/user.svg';
 import { ReactComponent as MainLogo } from '@/assets/mainLogo.svg';
 import { SideNavigation } from '@/components/common/Navigation/SideNavigation';
-import { SmallButton } from '@/components/common/SmallButton';
 import { NAVIGATION_MENU } from '@/constants/navigation';
 
 export const TopNavigation = () => {
@@ -17,81 +16,70 @@ export const TopNavigation = () => {
 
   return (
     <StyledContainer>
-      <StyledInnerContainer>
-        <Link to="/">
-          <MainLogo height="36px" />
-        </Link>
-        <StyledMenuContainer>
-          <StyledNavigationMenu>
-            {NAVIGATION_MENU.map((item) => (
-              <SmallButton
-                key={item.menu}
-                onClick={() => {
-                  navigate(item.path);
-                }}
-              >
-                {item.menu}
-              </SmallButton>
-            ))}
-          </StyledNavigationMenu>
-          {loggedIn ? (
-            <StyledUserButton>
-              <UserIcon />
-            </StyledUserButton>
-          ) : (
-            <SmallButton
-              filled
-              width="100px"
-              onClick={() => {
-                // TODO: 로그인 페이지로 이동하도록 수정
-                setLoggedIn((prev) => !prev);
-              }}
-            >
-              로그인
-            </SmallButton>
-          )}
-        </StyledMenuContainer>
-        <StyledMenuButton
-          onClick={() => {
-            //TODO: 마이페이지로 이동하도록 수정
-            setShowSideNav((prev) => !prev);
-          }}
-        >
-          <MenuIcon />
-        </StyledMenuButton>
-        {showSideNav && <SideNavigation isLoggedIn={loggedIn} setOpen={setShowSideNav} />}
-      </StyledInnerContainer>
+      <Link to="/">
+        <MainLogo height="36px" />
+      </Link>
+      <StyledMenuContainer>
+        {NAVIGATION_MENU.map((item) => (
+          <StyledMenuButton
+            key={item.menu}
+            onClick={() => {
+              navigate(item.path);
+            }}
+          >
+            {item.menu}
+          </StyledMenuButton>
+        ))}
+        {loggedIn ? (
+          <StyledUserButton>
+            <UserIcon />
+          </StyledUserButton>
+        ) : (
+          <StyledMenuButton
+            $filled
+            $width="100px"
+            onClick={() => {
+              // TODO: 로그인 페이지로 이동하도록 수정
+              setLoggedIn((prev) => !prev);
+            }}
+          >
+            로그인
+          </StyledMenuButton>
+        )}
+      </StyledMenuContainer>
+      <StyledSideNavButton
+        onClick={() => {
+          //TODO: 마이페이지로 이동하도록 수정
+          setShowSideNav((prev) => !prev);
+        }}
+      >
+        <MenuIcon />
+      </StyledSideNavButton>
+      {showSideNav && <SideNavigation isLoggedIn={loggedIn} setOpen={setShowSideNav} />}
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
   position: fixed;
   top: 0;
   left: 0;
   z-index: 10;
 
   width: 100%;
+  padding: 20px 42px;
 
   border-bottom: 1px solid ${({ theme }) => theme.color.primary100};
   background: rgba(255, 255, 255, 0.6);
   backdrop-filter: blur(5px);
 
   @media ${({ theme }) => theme.device.mobile} {
-    background: ${({ theme }) => theme.color.white};
-  }
-`;
-
-const StyledInnerContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  padding: 20px 42px;
-  margin: 0 auto;
-
-  @media ${({ theme }) => theme.device.mobile} {
     padding: 16px 8px 16px 24px;
+    background: ${({ theme }) => theme.color.white};
   }
 `;
 
@@ -105,11 +93,7 @@ const StyledMenuContainer = styled.div`
   }
 `;
 
-const StyledNavigationMenu = styled.div`
-  display: flex;
-`;
-
-const StyledMenuButton = styled.button`
+const StyledSideNavButton = styled.button`
   display: none;
   padding: 0px 12px;
 
@@ -127,4 +111,31 @@ const StyledUserButton = styled.button`
   path {
     fill: ${({ theme }) => theme.color.primary500};
   }
+`;
+
+const StyledMenuButton = styled.button<{ $filled?: boolean; $width?: string }>`
+  ${({ theme }) => theme.font.desktop.body2m};
+
+  width: ${({ $width }) => $width};
+  padding: 8px 24px;
+  border-radius: 8px;
+
+  ${(props) =>
+    props.$filled
+      ? css`
+          color: ${props.theme.color.white};
+          background: ${props.theme.color.gray800};
+
+          &:hover {
+            background: ${props.theme.color.gray700};
+          }
+        `
+      : css`
+          color: ${props.theme.color.gray800};
+          background: transparent;
+
+          &:hover {
+            color: ${props.theme.color.primary600};
+          }
+        `}
 `;
