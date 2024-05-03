de7edc7f90a5ef8c1b28415c19383e408de7d849
aaminha <dks4857@gmail.com>
2024-05-03 15:04:41 +0900

    feat: Carousel 컴포넌트 구현 (#9)

---
+++ src/components/common/Carousel/index.tsx
@@ -0,0 +1,42 @@
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+
+import { CarouselButton } from '@/components/common/Button/CarouselButton';
+
+const settings = {
+  infinite: false,
+  speed: 500,
+  slidesToScroll: 1,
+  nextArrow: <CarouselButton direction="next" />,
+  prevArrow: <CarouselButton direction="prev" />,
+  variableWidth: true,
+};
+
+interface CarouselProps {
+  gap?: string;
+  children: React.ReactNode;
+}
+
+export const Carousel = ({ gap = '24px', children }: CarouselProps) => {
+  return (
+    <SliderWrapper $gap={gap}>
+      <Slider {...settings}>{children}</Slider>
+    </SliderWrapper>
+  );
+};
+
+const SliderWrapper = styled.div<{ $gap: string }>`
+  .slick-list {
+    padding: 5px;
+
+    .slick-track {
+      display: flex;
+      gap: ${({ $gap }) => $gap};
+    }
+    .slick-track:before,
+    .slick-track:after {
+      content: none;
+    }
+  }
+`;
