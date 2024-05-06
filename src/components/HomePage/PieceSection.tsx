38e4d133cbbe44f0237dd84005977adf9bb5287a
aaminha <dks4857@gmail.com>
2024-05-06 17:11:40 +0900

    refactor: PieceSection 컴포넌트의 props 수정 및 디자인 코드 수정 (#9) - UserInformation 타입 추가

--- src/components/HomePage/PieceSection.tsx
+++ src/components/HomePage/PieceSection.tsx
@@ -3,51 +3,41 @@ import styled, { keyframes } from 'styled-components';
 import { ReactComponent as ArrowIcon } from '@/assets/icons/arrowDown.svg';
 import { Chip } from '@/components/common/Chip';
 import { SectionContainer } from '@/styles';
+import { UserInformation } from '@/types/user.type';
 
-const Dummy = {
-  piece: '크리에이터',
-  name: '민선',
-  brand: 'creator',
-  chips: [
-    { content: '감성적인', weight: 1.0 },
-    { content: '도전적인', weight: 0.9 },
-    { content: '탐색적인', weight: 0.8 },
-    { content: '활동적인', weight: 0.7 },
-    { content: '열정적인', weight: 0.6 },
-    { content: '현실적인', weight: 0.5 },
-    { content: '관습적인', weight: 0.45 },
-  ],
-};
+interface PieceSectionProps {
+  userInformation: UserInformation;
+}
 
-export const PieceSection = () => {
+export const PieceSection = ({ userInformation }: PieceSectionProps) => {
   return (
     <StyledContainer>
       <StyledSectionContainer>
         <StyledTitle>
-          <div className="user-info">
-            <span className="highlight">{Dummy.piece} 조각</span>을 가진{' '}
-            <span className="highlight">{Dummy.name}</span>님,
+          <div className="title">
+            <span className="highlight">{userInformation.piece} 조각</span>을 가진{' '}
+            <span className="highlight">{userInformation.name}</span>님,
           </div>
-          <div className="intro">셀피스와 함께 나를 브랜딩해봐요.</div>
+          <div className="subtitle">셀피스와 함께 나를 브랜딩해봐요.</div>
         </StyledTitle>
         <StyledCardContainer>
-          <StyledCard src={`/src/assets/cards/${Dummy.brand}.svg`} alt="card" />
-          {Dummy.chips.map((chip, index) => (
+          <img src={`/src/assets/cards/${userInformation.brand}.svg`} alt="card" />
+          {userInformation.chips.map((chip, index) => (
             <StyledBubble key={chip.content} className={`b${index}`} $weight={chip.weight}>
               <span>{chip.content}</span>
             </StyledBubble>
           ))}
         </StyledCardContainer>
         <StyledContents>
-          <StyledChipsContainer>
-            {Dummy.chips.map((chip) => (
+          <div className="chips">
+            {userInformation.chips.map((chip) => (
               <Chip key={chip.content} primary>
                 {chip.content}
               </Chip>
             ))}
-          </StyledChipsContainer>
+          </div>
           <div>
-            키워드를 가진 <span className="highlight">{Dummy.name}</span>님께
+            키워드를 가진 <span className="highlight">{userInformation.name}</span>님께
             <br />
             이런 경험을 추천드리고 싶어요.
           </div>
@@ -80,14 +70,14 @@ const StyledSectionContainer = styled(SectionContainer)`
 `;
 
 const StyledTitle = styled.div`
-  .user-info {
+  .title {
     margin-bottom: 16px;
 
     ${({ theme }) => theme.font.desktop.h1};
     color: ${({ theme }) => theme.color.black};
   }
 
-  .intro {
+  .subtitle {
     ${({ theme }) => theme.font.desktop.title1};
     color: ${({ theme }) => theme.color.gray600};
   }
@@ -98,26 +88,30 @@ const StyledCardContainer = styled.div`
 
   position: relative;
   z-index: 0;
+
+  img {
+    height: 440px;
+    margin: 56px 0;
+  }
 `;
 
-const StyledCard = styled.img`
-  height: 440px;
-  margin: 56px 0;
-`;
-
-const moveDown = keyframes`50% {
+const moveDown = keyframes`
+  50% {
     transform: translateY(-20px);
   }`;
 
-const moveUp = keyframes`50% {
+const moveUp = keyframes`
+  50% {
     transform: translateY(20px);
   }`;
 
-const moveRight = keyframes`50% {
+const moveRight = keyframes`
+  50% {
     transform: translateX(20px);
   }`;
 
-const moveLeft = keyframes`50% {
+const moveLeft = keyframes`
+  50% {
     transform: translateX(-20px);
   }`;
 
@@ -170,7 +164,7 @@ const StyledBubble = styled.div<{ $weight: number }>`
     left: 3%;
 
     animation:
-      ${moveDown} 2500ms ease-in-out infinite,
+      ${moveUp} 2500ms ease-in-out infinite,
       ${moveLeft} 3000ms ease-in-out infinite;
   }
 
@@ -211,18 +205,21 @@ const StyledBubble = styled.div<{ $weight: number }>`
   }
 `;
 
-const StyledChipsContainer = styled.div`
-  display: flex;
-  gap: 16px;
-
-  padding: 30px 0 38px 0;
-`;
-
 const StyledContents = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 24px;
 
+  padding-top: 30px;
+
   ${({ theme }) => theme.font.desktop.title1};
+  color: ${({ theme }) => theme.color.gray800};
+
+  .chips {
+    display: flex;
+    gap: 16px;
+
+    padding-bottom: 38px;
+  }
 `;
---
+++ src/types/user.type.ts
@@ -0,0 +1,6 @@
+export interface UserInformation {
+  piece: string;
+  name: string;
+  brand: string;
+  chips: { content: string; weight: number }[];
+}
