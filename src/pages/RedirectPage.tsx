de46a7f971dd4aaaea19c237ef48085765a6f88c
aaminha <dks4857@gmail.com>
2024-05-08 03:31:11 +0900

    feat: RedirectPage에 refresh_token을 받는 케이스 추가 (#14)

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -1,24 +1,35 @@
 import { useEffect } from 'react';
 
-import { useCookies } from 'react-cookie';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-const RedirectPage = () => {
+import { authService } from '@/services/AuthService';
+import { userService } from '@/services/UserService';
+
+export const RedirectPage = () => {
   const location = useLocation();
-  const [cookies, setCookie] = useCookies(['access_token']);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
+
+    const nickname = params.get('nickname');
     const accessToken = params.get('access_token');
+    const registerToken = params.get('register_token');
+
+    if (registerToken) {
+      authService.onLoginSuccess(registerToken);
+      userService.setUser({ nickname: '' });
+      navigate('/'); // TODO: 온보딩 화면으로 이동하도록 수정
+    }
 
     if (accessToken) {
-      const expireTime = new Date(Date.now() + import.meta.env.VITE_ACCESS_TOKEN_EXPIRATION_TIME);
-      setCookie('access_token', accessToken, { expires: expireTime });
-      window.location.href = '/';
+      authService.onLoginSuccess(accessToken);
+      nickname && userService.setUser({ nickname });
+      navigate('/');
     }
-  }, [location.search, setCookie, cookies]);
+    // TODO: warning 해결
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return null;
 };
-
-export default RedirectPage;
