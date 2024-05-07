74f70a13944c1fb781fd7bccfaedb1702415e194
aaminha <dks4857@gmail.com>
2024-05-08 03:30:20 +0900

    feat: authClient 이름의 axios client 생성 (#14)

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -1,6 +1,28 @@
 import axios, { AxiosInstance } from 'axios';
 
+import { authService } from '@/services/AuthService';
+import { userService } from '@/services/UserService';
+
 export const noAuthClient: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+});
+
+export const authClient: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
+
+authClient.interceptors.request.use((config) => {
+  if (!authClient.defaults.headers.common['Authorization']) {
+    if (userService.getUser() === '') {
+      // register token이 만료된 상황
+      window.location.href = '/auth';
+    } else if (userService.getUser()) {
+      // access token이 만료된 상황
+      authService.onRefreshToken();
+    }
+  }
+
+  return config;
 });
