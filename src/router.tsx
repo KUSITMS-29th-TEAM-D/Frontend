db9af57975fe9cd3fc65840080401be4d9cc4486
aaminha <dks4857@gmail.com>
2024-05-08 11:03:11 +0900

    fix: 최소 너비가 1280이 되도록 layout 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { MainLayout } from '@/components/common/Layout/MainLayout';
+import { ResponsiveLayout } from '@/components/common/Layout/ResponsiveLayout';
 import { HomePage } from '@/pages/HomePage';
 import { LoginPage } from '@/pages/LoginPage';
 import { RedirectPage } from '@/pages/RedirectPage';
@@ -10,9 +11,11 @@ export const Router = () => {
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<RedirectPage />} />
+      </Route>
+      <Route element={<ResponsiveLayout />}>
         <Route path="/auth" element={<LoginPage />} />
       </Route>
+      <Route path="/login" element={<RedirectPage />} />
     </Routes>
   );
 };
