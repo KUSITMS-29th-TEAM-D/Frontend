197f36d7ee173da74e578efc6b8f6831cd09346d
aaminha <dks4857@gmail.com>
2024-05-16 03:49:07 +0900

    fix: KeywordChip 컴포넌트 수정 (#26) - 삭제 아이콘 제거 - selected의 hover 디자인 추가

--- src/components/common/Chip/KeywordChip.tsx
+++ src/components/common/Chip/KeywordChip.tsx
@@ -1,7 +1,5 @@
 import styled, { css } from 'styled-components';
 
-import { ReactComponent as DeleteIcon } from '@/assets/icons/close.svg';
-
 interface KeywordChipProps {
   selected?: boolean;
   children: string;
@@ -20,15 +18,15 @@ export const KeywordChip = ({
       $selected={selected}
       onClick={() => {
         if (!selected) selectHandler && selectHandler(children);
+        else deleteHandler && deleteHandler(children);
       }}
     >
       {children}
-      {selected && <StyledDeleteIcon onClick={() => deleteHandler && deleteHandler(children)} />}
     </StyledKeywordChip>
   );
 };
 
-const StyledKeywordChip = styled.div<{ $selected: boolean }>`
+const StyledKeywordChip = styled.button<{ $selected: boolean }>`
   display: flex;
   align-items: center;
   gap: 6px;
@@ -37,33 +35,26 @@ const StyledKeywordChip = styled.div<{ $selected: boolean }>`
   padding: 7px 16px;
 
   ${({ theme }) => theme.font.desktop.label1m};
-  color: ${({ theme }) => theme.color.primary700};
-  text-align: center;
 
+  text-align: center;
   border-radius: 8px;
-  background: ${({ theme }) => theme.color.white};
-  cursor: default;
 
   ${({ $selected, theme }) =>
     $selected
       ? css`
-          padding: 7px 12px;
           background: ${theme.color.primary500};
           color: ${theme.color.white};
+
+          &:hover {
+            background: ${theme.color.primary600};
+          }
         `
       : css`
+          background: ${({ theme }) => theme.color.white};
+          color: ${({ theme }) => theme.color.primary700};
+
           &:hover {
             background: ${({ theme }) => theme.color.primary50};
-            cursor: pointer;
           }
         `};
-`;
-
-const StyledDeleteIcon = styled(DeleteIcon)`
-  width: 16px;
-  height: 16px;
-  cursor: pointer;
-  path {
-    fill: ${({ theme }) => theme.color.white};
-  }
 `;
