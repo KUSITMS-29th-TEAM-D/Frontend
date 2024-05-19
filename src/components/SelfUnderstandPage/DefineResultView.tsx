af16e7b5dfcdf4361e08d932fceba331d7ab4ae9
aaminha <dks4857@gmail.com>
2024-05-19 21:27:41 +0900

    feat: Define 테스트 결과 케이스 추가 (#49)

--- src/components/SelfUnderstandPage/DefineResultView.tsx
+++ src/components/SelfUnderstandPage/DefineResultView.tsx
@@ -1,24 +1,38 @@
 import { useEffect, useState } from 'react';
 
+import styled from 'styled-components';
+
 import { personaAPI } from '@/apis/personaAPI';
+import { ResultView } from '@/components/DefineResultPage/ResultView';
 import { NoResultSection } from '@/components/SelfUnderstandPage/NoResultTemplate';
+import { userService } from '@/services/UserService';
 import { DefineResult } from '@/types/test.type';
 
 export const DefineResultView = () => {
   const [defineResult, setDefineResult] = useState<DefineResult | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await personaAPI.getPersonaMember();
-      if (result) {
-        setDefineResult(result);
-      }
-    };
-
-    fetchData();
+    if (userService.getUserState() === 'MEMBER') {
+      personaAPI.getPersonaMember().then((res) => {
+        setDefineResult(res.payload);
+      });
+    }
   }, []);
 
-  if (defineResult) return null;
+  if (defineResult)
+    return (
+      <StyledContainer>
+        <ResultView result={defineResult} />
+      </StyledContainer>
+    );
 
   return <NoResultSection tab="Define" />;
 };
+
+const StyledContainer = styled.div`
+  background: ${({ theme }) =>
+    `linear-gradient(180deg, ${theme.color.white} 0%, ${theme.color.primary100} 100%)`};
+
+  width: 100%;
+  padding: 0 64px 40px 64px;
+`;
