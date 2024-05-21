a4f7e7b62978713b3ac7d1807c26078116e9e1f9
aaminha <dks4857@gmail.com>
2024-05-21 12:40:48 +0900

    feat: 돌아보기 챗봇 화면 구현 (#37)

--- src/pages/DiscoverTestPage.tsx
+++ src/pages/DiscoverTestPage.tsx
@@ -1,3 +1,36 @@
+import styled from 'styled-components';
+
+import { ChattingBox } from '@/components/DiscoverTestPage/ChattingBox';
+import { RightSidebar } from '@/components/DiscoverTestPage/RightSidebar';
+
 export const DiscoverTestPage = () => {
-  return <div>DiscoverTestPage</div>;
+  return (
+    <>
+      <StyledContainer>
+        <StyledInnerContainer>
+          <ChattingBox />
+          <RightSidebar />
+        </StyledInnerContainer>
+      </StyledContainer>
+    </>
+  );
 };
+
+const StyledContainer = styled.div`
+  min-width: max-content;
+  height: 100vh;
+  padding: 76px 64px 24px 64px;
+  overflow: hidden;
+`;
+
+const StyledInnerContainer = styled.div`
+  width: fit-content;
+  height: 100%;
+
+  margin: 0 auto;
+  overflow: hidden;
+
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
