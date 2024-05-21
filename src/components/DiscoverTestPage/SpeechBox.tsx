ed75fb70035e20f98ee62ff3bdba16690bdaec66
aaminha <dks4857@gmail.com>
2024-05-21 12:39:23 +0900

    fix: SpeechBox 연속 채팅 관련 디자인 추가 (#37)

--- src/components/DiscoverTestPage/SpeechBox.tsx
+++ src/components/DiscoverTestPage/SpeechBox.tsx
@@ -1,15 +1,67 @@
 import styled, { css } from 'styled-components';
 
+import ChatBotProfile from '@/assets/icons/chatbot.png';
+import { ReactComponent as User } from '@/assets/icons/user.svg';
+
 interface SpeechBoxProps {
   children: string;
   isUser?: boolean;
+  isContinuous: boolean;
 }
 
-export const SpeechBox = ({ children, isUser = false }: SpeechBoxProps) => {
-  return <StyledContainer $isUser={isUser}>{children}</StyledContainer>;
+export const SpeechBox = ({ children, isUser = false, isContinuous }: SpeechBoxProps) => {
+  const userNickname = '유저이름';
+
+  return (
+    <StyledContainer $isUser={isUser}>
+      {isContinuous ? (
+        <div style={{ width: '40px' }} />
+      ) : isUser ? (
+        <UserProfile>
+          <User />
+        </UserProfile>
+      ) : (
+        <img src={ChatBotProfile} alt="profile" width={40} height={40} />
+      )}
+      <div className="text-container">
+        {!isContinuous && <span>{isUser ? userNickname : '셀퍼'}</span>}
+        <StyledTextBox $isUser={isUser} $continuous={isContinuous}>
+          {children}
+        </StyledTextBox>
+      </div>
+    </StyledContainer>
+  );
 };
 
 const StyledContainer = styled.div<{ $isUser: boolean }>`
+  display: flex;
+  flex-direction: ${({ $isUser }) => ($isUser ? 'row-reverse' : 'row')};
+  gap: 12px;
+
+  .text-container {
+    text-align: ${({ $isUser }) => ($isUser ? 'right' : 'left')};
+
+    span {
+      ${({ theme }) => theme.font.desktop.label1m};
+      color: ${({ theme }) => theme.color.gray800};
+    }
+  }
+`;
+
+const UserProfile = styled.div`
+  width: fit-content;
+  height: fit-content;
+
+  padding: 8px;
+  border-radius: 8px;
+  background: ${({ theme }) => theme.color.primary50};
+
+  svg path {
+    fill: ${({ theme }) => theme.color.primary500};
+  }
+`;
+
+const StyledTextBox = styled.div<{ $isUser: boolean; $continuous: boolean }>`
   width: fit-content;
   max-width: 550px;
   padding: 10px;
@@ -32,4 +84,6 @@ const StyledContainer = styled.div<{ $isUser: boolean }>`
           border-color: ${({ theme }) => theme.color.gray150};
           background: ${({ theme }) => theme.color.white};
         `}
+
+  margin-bottom: ${({ $continuous }) => ($continuous ? '24px' : '8px')};
 `;
