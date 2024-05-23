ce3cdfed10661b7c2339fa10ddce889c01af2544
Mungjin01 <beckaljm@naver.com>
2024-05-23 16:41:09 +0900

    chore: recommend에서 program으로 워딩 변경 (#20)

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -3,9 +3,8 @@ import DesignIcon from '@/assets/icons/designBlack.png';
 import DiscoverIcon from '@/assets/icons/discoverBlack.png';
 
 export const NAVIGATION_MENU = [
-  //TODO: path 수정하기
   { menu: '자기이해', path: '/understand' },
-  { menu: '경험추천', path: '/recommend' },
+  { menu: '프로그램', path: '/program' },
 ];
 
 export const MY_UNDERSTAND_TAB = [
